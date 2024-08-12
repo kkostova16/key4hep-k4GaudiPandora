4405d76454920a0925fa9e9f243fdeda4efe6cfb
Katerina Kostova <kkostova@pcepsft11.dyndns.cern.ch>
2024-08-12 14:53:29 +0200

    Add steering file for DDCaloDigi

---
+++ k4GaudiPandora/options/runDDCaloDigi.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) 2020-2024 Key4hep-Project.
+#
+# This file is part of Key4hep.
+# See https://key4hep.github.io/key4hep-doc/ for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from Gaudi.Configuration import INFO
+from k4FWCore import ApplicationMgr, IOSvc
+from Configurables import EventDataSvc
+from Configurables import DDCaloDigi
+
+from Configurables import GeoSvc
+from Configurables import UniqueIDGenSvc
+from Configurables import RootHistSvc
+from Configurables import Gaudi__Histograming__Sink__Root as RootHistoSink
+import os
+
+id_service = UniqueIDGenSvc("UniqueIDGenSvc")
+
+geoservice = GeoSvc("GeoSvc")
+geoservice.detectors = [os.environ["K4GEO"]+"/FCCee/CLD/compact/CLD_o2_v06/CLD_o2_v06.xml"]
+geoservice.OutputLevel = INFO
+geoservice.EnableGeant4Geo = False
+
+calodigi = DDCaloDigi()
+
+#FIXME: add properties
+
+iosvc = IOSvc()
+iosvc.input = "../simulation/sim_partgun_1000.root"
+iosvc.output = "../outputfiles/outputCaloDigi_Gaudi.root"
+
+hps = RootHistSvc("HistogramPersistencySvc")
+root_hist_svc = RootHistoSink("RootHistoSink")
+root_hist_svc.FileName = "../outputfiles/ddcalodigi_hist.root"
+
+ApplicationMgr(TopAlg=[calodigi],
+               EvtSel="NONE",
+               EvtMax=-1,
+               ExtSvc=[EventDataSvc("EventDataSvc"), root_hist_svc],
+               OutputLevel=INFO,
+               )
